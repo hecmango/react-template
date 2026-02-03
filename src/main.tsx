9d8dc29a5f0f8d8d1e69df19da44cb2896987def
Hector <hamancia@innovacion.gob.sv>
2026-02-03 09:56:38 -0600

    Installation of react-router-don, sass, zuztand, tailwind and primereact

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { PrimeReactProvider } from 'primereact/api'
+import { RouterProvider } from 'react-router-dom';
+
+import {router} from './router/index.tsx';
+
+import './styles/tailwind.css'
+import 'primereact/resources/themes/mira/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
+import './styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider>
+        <RouterProvider router={router}/>
+    </PrimeReactProvider>
   </StrictMode>,
 )
